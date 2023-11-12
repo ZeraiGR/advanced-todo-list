1b94b5554b2c45fdb69d66206556733bae068177
ZeraiGR <kirillgr15@mail.ru>
2023-11-12 15:54:59 +0300

    feature(register): add register controller

---
+++ backend/src/controllers/auth/register.ts
@@ -0,0 +1,62 @@
+import type { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
+import type { UserRegisterDto } from '../../types';
+
+import {
+  createSession,
+  createUser,
+  hasUserWithEmail,
+  hasUserWithNickname,
+  hasUserWithPhone,
+} from '../../services/auth';
+import { createIpAccountLimit, getAccountLimitByIp, updateIpAccountLimit } from '../../repositories/auth';
+
+const IP_ACCOUNT_LIMIT = Number(process.env.IP_ACCOUNT_LIMIT);
+
+export const register = async (fastify: FastifyInstance, request: FastifyRequest, reply: FastifyReply) => {
+  const body = request.body as UserRegisterDto;
+
+  const { nickName, phone, email, password, confirmPassword } = body;
+  const ip = request.ip;
+
+  if (password !== confirmPassword) {
+    await request.session.destroy();
+    return reply.status(400).send({ message: 'Пароли не совпадают' });
+  }
+
+  if (await hasUserWithNickname(nickName)) {
+    await request.session.destroy();
+    return reply.status(400).send({ message: 'Пользователь c таким nickName уже существует' });
+  }
+
+  if (await hasUserWithEmail(email)) {
+    await request.session.destroy();
+    return reply.status(400).send({ message: 'Пользователь c таким email уже существует' });
+  }
+
+  if (phone && (await hasUserWithPhone(phone))) {
+    await request.session.destroy();
+    return reply.status(400).send({ message: 'Пользователь c таким номером телефона уже существует' });
+  }
+
+  // Убеждаемся, что у пользователя под уникальлный ip не более 2 учетных записей
+  const accountLimit = await getAccountLimitByIp(ip);
+
+  if (!accountLimit) {
+    await createIpAccountLimit(ip);
+  } else {
+    if (accountLimit.accounts >= IP_ACCOUNT_LIMIT) {
+      await request.session.destroy();
+      return reply.code(403).send({ message: 'Превышен лимит аккаунтов' });
+    } else {
+      await updateIpAccountLimit(ip);
+    }
+  }
+
+  const user = await createUser(body);
+  fastify.log.info('A new user was created:', user);
+
+  await createSession(request, ip, user.id);
+  fastify.log.info('A new session was created');
+
+  return { id: user.id };
+};
